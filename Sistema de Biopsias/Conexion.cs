2c88ac9d79ac819756bcff6d1a340b1f485dd2c3
HPSCOMPUTOS <109764511+HPSCOMPUTOS@users.noreply.github.com>
2022-12-21 11:13:21 -0300

    11.13 servidor

---
+++ Sistema de Biopsias/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Sistema_de_Biopsias
+{
+    public class Conexion
+    {
+
+        public static SqlConnection Conectar() 
+        {
+            SqlConnection cn = new SqlConnection("SERVER=WIN-9FKBBGT25H3\\SQLEXPRESS;DATABASE=Biopsys;Integrated security=true");
+            cn.Open();
+            return cn;
+        }
+
+    }
+}
